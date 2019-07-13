546e666daff7f93925ecf2e517b24c06ad68d32c
prashant9316 <48996523+prashant9316@users.noreply.github.com>
2019-07-13 18:57:36 +0530

    Add files via upload

---
+++ Neo 6m GPS/Neo 6m GPS.ino
@@ -0,0 +1,19 @@
+
+ 
+#include <SoftwareSerial.h>
+
+// The serial connection to the GPS module
+SoftwareSerial gps(4, 3);
+
+void setup(){
+  Serial.begin(9600);
+  gps.begin(9600);
+}
+
+void loop(){
+  while (gpss.available() > 0){
+    // get the byte data from the GPS
+    byte gpsData = gps.read();
+    Serial.write(gpsData);
+  }
+}
